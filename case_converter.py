84fbbb889533c30a7d8857ddacfad54054fba16a
ceianreigh <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-10-16 15:17:19 +0800

    refactor the function to implement list comprehension

--- case_converter.py
+++ case_converter.py
@@ -1,18 +1,20 @@
 # learning python list comprehension by building a case converter program
 
 def convert_to_snake_case(pascal_or_camel_cased_string):
-    snake_cased_char_list = []
+    # snake_cased_char_list = []
     
-    for char in pascal_or_camel_cased_string:
-        if char.isupper():
-            converted_character = '_' + char.lower()
-            snake_cased_char_list.append(converted_character)
-        else:
-            snake_cased_char_list.append(char)
-    snake_cased_string = ''.join(snake_cased_char_list)
-    cleaned_snake_cased_string = snake_cased_string.strip('_')  
-    return cleaned_snake_cased_string
+    # for char in pascal_or_camel_cased_string:
+    #     if char.isupper():
+    #         converted_character = '_' + char.lower()
+    #         snake_cased_char_list.append(converted_character)
+    #     else:
+    #         snake_cased_char_list.append(char)
+    # snake_cased_string = ''.join(snake_cased_char_list)
+    # cleaned_snake_cased_string = snake_cased_string.strip('_')  
+    # return cleaned_snake_cased_string
 
+    snake_cased_char_list = []
+    return ''.join(snake_cased_char_list).strip('')
 def main():
     print(convert_to_snake_case('aLongAndComplexString'))
 
