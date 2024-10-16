85fb02d02b786e0b3f2df017f0c147f42e46306b
ceianreigh <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-10-16 15:18:33 +0800

    simplify the commented code into a list comprehension

--- case_converter.py
+++ case_converter.py
@@ -13,7 +13,8 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     # cleaned_snake_cased_string = snake_cased_string.strip('_')  
     # return cleaned_snake_cased_string
 
-    snake_cased_char_list = []
+    snake_cased_char_list = ['_' + char.lower() if char.isupper()
+                             else char for char in pascal_or_camel_cased_string]
     return ''.join(snake_cased_char_list).strip('')
 def main():
     print(convert_to_snake_case('aLongAndComplexString'))
